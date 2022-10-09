9f94f81f1b58a6ba37846046a92e815578554c06
aleja <92136921+mariather@users.noreply.github.com>
2022-10-08 23:38:14 -0700

    create login and logout request

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,5 +21,55 @@ router.post('/', async (req, res) => {
   }
 });
 
-// add Login
-// add Logout
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again!' });
+      return;
+    }
+
+    const user = dbUserData.get({ plain: true })
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.user_id = user.id;
+
+      res
+        .status(200)
+        .json({ user: user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
